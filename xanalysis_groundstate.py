76924267e76d83826c8e96b7efed7fd335731445
Kushal Seetharam <kushalseetharam@gmail.com>
2018-04-25 11:55:45 -0400

    fixed phonon dist

--- xanalysis_groundstate.py
+++ xanalysis_groundstate.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 
     # Toggle parameters
 
-    toggleDict = {'Location': 'home', 'Dynamics': 'imaginary', 'Interaction': 'on', 'Grid': 'spherical', 'Coupling': 'twophonon'}
+    toggleDict = {'Location': 'work', 'Dynamics': 'imaginary', 'Interaction': 'on', 'Grid': 'spherical', 'Coupling': 'twophonon'}
 
     # ---- SET OUTPUT DATA FOLDER ----
 
@@ -166,8 +166,15 @@ if __name__ == "__main__":
     # kgrid = Grid.Grid("SPHERICAL_2D"); kgrid.initArray_premade('k', CSAmp_ds.coords['k'].values); kgrid.initArray_premade('th', CSAmp_ds.coords['th'].values)
     # kVec = kgrid.getArray('k')
     # thVec = kgrid.getArray('th')
+    # list_of_unit_vectors = list(kgrid.arrays.keys())
+    # list_of_functions = [lambda k: (2 * np.pi)**(-2) * k**2, np.sin]
+    # sphfac = kgrid.function_prod(list_of_unit_vectors, list_of_functions)
+    # kDiff = kgrid.diffArray('k')
+    # thDiff = kgrid.diffArray('th')
 
     # kAve_Vals = np.zeros(PVals.size)
+    # thAve_Vals = np.zeros(PVals.size)
+    # kFWHM_Vals = np.zeros(PVals.size)
     # thFWHM_Vals = np.zeros(PVals.size)
     # PhDen_k_Vec = np.empty(PVals.size, dtype=np.object)
     # PhDen_th_Vec = np.empty(PVals.size, dtype=np.object)
@@ -175,31 +182,42 @@ if __name__ == "__main__":
     # for Pind, P in enumerate(PVals):
     #     CSAmp = CSAmp_ds_inf.sel(P=P).values
     #     Nph = qds_aIBi.isel(t=-1).sel(P=P)['Nph'].values
-    #     PhDen = (1 / Nph) * kgrid.dV() * np.abs(CSAmp.reshape(CSAmp.size))**2
-    #     PhDen_k = kgrid.integrateFunc(PhDen, 'th'); PhDen_k_Vec[Pind] = PhDen_k
-    #     PhDen_th = kgrid.integrateFunc(PhDen, 'k'); PhDen_th_Vec[Pind] = PhDen_th
-    #     kAve_Vals[Pind] = np.dot(kVec, PhDen_k)
+    #     PhDen = (1 / Nph) * sphfac * np.abs(CSAmp.reshape(CSAmp.size))**2
+
+    #     PhDen_mat = PhDen.reshape((len(kVec), len(thVec)))
+    #     PhDen_k = np.dot(PhDen_mat, thDiff); PhDen_k_Vec[Pind] = PhDen_k
+    #     PhDen_th = np.dot(np.transpose(PhDen_mat), kDiff); PhDen_th_Vec[Pind] = PhDen_th
+
+    #     # PhDen_k = kgrid.integrateFunc(PhDen, 'th'); PhDen_k_Vec[Pind] = PhDen_k
+    #     # PhDen_th = kgrid.integrateFunc(PhDen, 'k'); PhDen_th_Vec[Pind] = PhDen_th
+
+    #     kAve_Vals[Pind] = np.dot(kVec, PhDen_k * kDiff)
+    #     thAve_Vals[Pind] = np.dot(thVec, PhDen_th * thDiff)
+
+    #     kFWHM_Vals[Pind] = pfc.FWHM(kVec, PhDen_k)
     #     thFWHM_Vals[Pind] = pfc.FWHM(thVec, PhDen_th)
 
     # fig1, ax1 = plt.subplots(1, 2)
-    # ax1[0].plot(PVals, kAve_Vals, 'b-')
-    # ax1[1].plot(PVals, thFWHM_Vals, 'g-')
+    # ax1[0].plot(PVals, kAve_Vals, 'b-', label='Mean')
+    # ax1[0].plot(PVals, kFWHM_Vals, 'g-', label='FWHM')
+    # ax1[0].legend()
     # ax1[0].set_xlabel('P')
-    # ax1[0].set_ylabel('Average ' + r'$|\vec{k}|$')
-    # ax1[0].set_title('Mean of ' + r'$|\vec{k}|$' + ' Distribution of Emitted Phonons (' + r'$aIB^{-1}=$' + '{0})'.format(aIBi))
-    # ax1[1].set_xlabel('P')
-    # ax1[1].set_ylabel('FWHM of ' + r'$\theta$')
-    # ax1[1].set_title('Breadth of ' + r'$\theta$' + ' Distribution of Emitted Phonons (' + r'$aIB^{-1}=$' + '{0})'.format(aIBi))
+    # ax1[0].set_title('Characteristics of ' + r'$|\vec{k}|$' + ' Distribution of Individual Phonons (' + r'$aIB^{-1}=$' + '{0})'.format(aIBi))
 
-    # plt.show()
+    # ax1[1].plot(PVals, thAve_Vals, 'b-', label='Mean')
+    # ax1[1].plot(PVals, thFWHM_Vals, 'g-', label='FWHM')
+    # ax1[1].legend()
+    # ax1[1].set_xlabel('P')
+    # ax1[1].set_title('Characteristics of ' + r'$\theta$' + ' Distribution of Individual Phonons (' + r'$aIB^{-1}=$' + '{0})'.format(aIBi))
 
     # fig2, ax2 = plt.subplots()
     # curve2 = ax2.plot(kVec, PhDen_k_Vec[0], color='g', lw=2)[0]
     # P_text2 = ax2.text(0.85, 0.9, 'P: {:.2f}'.format(PVals[0]), transform=ax2.transAxes, color='r')
     # ax2.set_xlim([-0.01, np.max(kVec)])
-    # ax2.set_ylim([0, .005])
-    # ax2.set_title('Phonon Momentum Magnitude Distribution (' + r'$aIB^{-1}=$' + '{0})'.format(aIBi))
-    # ax2.set_ylabel(r'$\int n_{\vec{k}} \cdot \sin(\theta) d\theta$' + '  where  ' + r'$n_{\vec{k}}=\frac{1}{N_{ph}}|\beta_{\vec{k}}|^{2} dV_{\vec{k}}$')
+    # ax2.set_ylim([0, 5])
+    # ax2.set_title('Individual Phonon Momentum Magnitude Distribution (' + r'$aIB^{-1}=$' + '{0})'.format(aIBi))
+    # ax2.set_ylabel(r'$\int n_{\vec{k}} \cdot d\theta$' + '  where  ' + r'$n_{\vec{k}}=\frac{1}{N_{ph}}|\beta_{\vec{k}}|^{2} |\vec{k}|^{2} \sin(\theta)$')
+
     # ax2.set_xlabel(r'$|\vec{k}|$')
 
     # def animate2(i):
@@ -212,10 +230,10 @@ if __name__ == "__main__":
     # curve3 = ax3.plot(thVec, PhDen_th_Vec[0], color='g', lw=2)[0]
     # P_text3 = ax3.text(0.85, 0.9, 'P: {:.2f}'.format(PVals[0]), transform=ax3.transAxes, color='r')
     # ax3.set_xlim([-0.01, np.max(thVec)])
-    # ax3.set_ylim([0, .0005])
-    # ax3.set_title('Phonon Momentum Direction Distribution (' + r'$aIB^{-1}=$' + '{0})'.format(aIBi))
-    # ax3.set_ylabel(r'$\int n_{\vec{k}} \cdot k^{2} dk$' + '  where  ' + r'$n_{\vec{k}}=\frac{1}{N_{ph}}|\beta_{\vec{k}}|^{2} dV_{\vec{k}}$')
-    # ax3.set_xlabel(r'$|\vec{k}|$')
+    # ax3.set_ylim([0, 5])
+    # ax3.set_title('Individual Phonon Momentum Direction Distribution (' + r'$aIB^{-1}=$' + '{0})'.format(aIBi))
+    # ax3.set_ylabel(r'$\int n_{\vec{k}} \cdot d|\vec{k}|$' + '  where  ' + r'$n_{\vec{k}}=\frac{1}{N_{ph}}|\beta_{\vec{k}}|^{2} |\vec{k}|^{2} \sin(\theta)$')
+    # ax3.set_xlabel(r'$\theta$')
 
     # def animate3(i):
     #     curve3.set_ydata(PhDen_th_Vec[i])
@@ -223,8 +241,8 @@ if __name__ == "__main__":
     # anim3 = FuncAnimation(fig3, animate3, interval=1000, frames=range(PVals.size))
     # anim3.save(animpath + '/aIBi_{0}'.format(aIBi) + '_PhononDist_theta.gif', writer='imagemagick')
 
-    # # plt.draw()
-    # # plt.show()
+    # plt.draw()
+    # plt.show()
 
     # # GROUND STATE DISTRIBUTION CHARACTERIZATION (CARTESIAN)
 
