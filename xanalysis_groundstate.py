17f9e346f428fc24d9c286d12b7d9642b8474ca6
Kushal Seetharam <kushalseetharam@gmail.com>
2018-05-10 15:44:41 -0400

    plots

--- xanalysis_groundstate.py
+++ xanalysis_groundstate.py
@@ -199,46 +199,47 @@ if __name__ == "__main__":
 
     # # POLARON SOUND VELOCITY (SPHERICAL)
 
-    # # Check to see if linear part of polaron (total system) energy spectrum has slope equal to sound velocity
+    # Check to see if linear part of polaron (total system) energy spectrum has slope equal to sound velocity
 
-    # aIBi_Vals = qds.coords['aIBi'].values
-    # vsound_Vals = np.zeros(aIBi_Vals.size)
-    # vI_Vals = np.zeros(aIBi_Vals.size)
-    # for aind, aIBi in enumerate(aIBi_Vals):
-    #     qds_aIBi = qds.sel(aIBi=aIBi).isel(t=-1)
-    #     CSAmp_ds = qds_aIBi['Real_CSAmp'] + 1j * qds_aIBi['Imag_CSAmp']
-    #     kgrid = Grid.Grid("SPHERICAL_2D"); kgrid.initArray_premade('k', CSAmp_ds.coords['k'].values); kgrid.initArray_premade('th', CSAmp_ds.coords['th'].values)
-    #     Energy_Vals_inf = np.zeros(PVals.size)
-    #     PI_Vals = np.zeros(PVals.size)
-    #     for Pind, P in enumerate(PVals):
-    #         CSAmp = CSAmp_ds.sel(P=P).values
-    #         Energy_Vals_inf[Pind] = pfs.Energy(CSAmp, kgrid, P, aIBi, mI, mB, n0, gBB)
-    #         PI_Vals[Pind] = P - qds_aIBi.sel(P=P)['Pph'].values
+    aIBi_Vals = qds.coords['aIBi'].values
+    vsound_Vals = np.zeros(aIBi_Vals.size)
+    vI_Vals = np.zeros(aIBi_Vals.size)
+    for aind, aIBi in enumerate(aIBi_Vals):
+        qds_aIBi = qds.sel(aIBi=aIBi).isel(t=-1)
+        CSAmp_ds = qds_aIBi['Real_CSAmp'] + 1j * qds_aIBi['Imag_CSAmp']
+        kgrid = Grid.Grid("SPHERICAL_2D"); kgrid.initArray_premade('k', CSAmp_ds.coords['k'].values); kgrid.initArray_premade('th', CSAmp_ds.coords['th'].values)
+        Energy_Vals_inf = np.zeros(PVals.size)
+        PI_Vals = np.zeros(PVals.size)
+        for Pind, P in enumerate(PVals):
+            CSAmp = CSAmp_ds.sel(P=P).values
+            Energy_Vals_inf[Pind] = pfs.Energy(CSAmp, kgrid, P, aIBi, mI, mB, n0, gBB)
+            PI_Vals[Pind] = P - qds_aIBi.sel(P=P)['Pph'].values
 
-    #     Einf_tck = interpolate.splrep(PVals, Energy_Vals_inf, s=0)
-    #     Pinf_Vals = np.linspace(np.min(PVals), np.max(PVals), 5 * PVals.size)
-    #     Einf_Vals = 1 * interpolate.splev(Pinf_Vals, Einf_tck, der=0)
-    #     Einf_2ndderiv_Vals = 1 * interpolate.splev(Pinf_Vals, Einf_tck, der=2)
+        Einf_tck = interpolate.splrep(PVals, Energy_Vals_inf, s=0)
+        Pinf_Vals = np.linspace(np.min(PVals), np.max(PVals), 5 * PVals.size)
+        Einf_Vals = 1 * interpolate.splev(Pinf_Vals, Einf_tck, der=0)
+        Einf_2ndderiv_Vals = 1 * interpolate.splev(Pinf_Vals, Einf_tck, der=2)
 
-    #     sound_mask = np.abs(Einf_2ndderiv_Vals) <= 5e-3
-    #     Einf_sound = Einf_Vals[sound_mask]
-    #     Pinf_sound = Pinf_Vals[sound_mask]
-    #     [vsound_Vals[aind], vs_const] = np.polyfit(Pinf_sound, Einf_sound, deg=1)
+        sound_mask = np.abs(Einf_2ndderiv_Vals) <= 5e-3
+        Einf_sound = Einf_Vals[sound_mask]
+        Pinf_sound = Pinf_Vals[sound_mask]
+        [vsound_Vals[aind], vs_const] = np.polyfit(Pinf_sound, Einf_sound, deg=1)
 
-    #     vI_inf_tck = interpolate.splrep(PVals, PI_Vals / mI, s=0)
-    #     vI_inf_Vals = 1 * interpolate.splev(Pinf_Vals, vI_inf_tck, der=0)
-    #     vI_Vals[aind] = np.polyfit(Pinf_sound, vI_inf_Vals[sound_mask], deg=0)
+        vI_inf_tck = interpolate.splrep(PVals, PI_Vals / mI, s=0)
+        vI_inf_Vals = 1 * interpolate.splev(Pinf_Vals, vI_inf_tck, der=0)
+        vI_Vals[aind] = np.polyfit(Pinf_sound, vI_inf_Vals[sound_mask], deg=0)
 
-    # print(vsound_Vals)
-    # print(100 * (vsound_Vals - nu) / nu)
-    # fig, ax = plt.subplots()
-    # ax.plot(aIBi_Vals, vsound_Vals, 'ro', label='Post-Transition Polaron Sound Velocity (' + r'$\frac{\partial E}{\partial P}$' + ')')
-    # ax.plot(aIBi_Vals, vI_Vals, 'go', label='Post-Transition Impurity Velocity (' + r'$\frac{P-P_{ph}}{m_{I}}$' + ')')
-    # ax.plot(aIBi_Vals, nu * np.ones(aIBi_Vals.size), 'k--', label='BEC Sound Speed')
-    # ax.legend()
-    # ax.set_title('Velocity Comparison')
-    # ax.set_xlabel(r'$a_{IB}^{-1}$')
-    # plt.show()
+    print(vsound_Vals)
+    print(100 * (vsound_Vals - nu) / nu)
+    fig, ax = plt.subplots()
+    ax.plot(aIBi_Vals, vsound_Vals, 'ro', label='Post-Transition Polaron Sound Velocity (' + r'$\frac{\partial E}{\partial P}$' + ')')
+    ax.plot(aIBi_Vals, vI_Vals, 'go', label='Post-Transition Impurity Velocity (' + r'$\frac{P-P_{ph}}{m_{I}}$' + ')')
+    ax.plot(aIBi_Vals, nu * np.ones(aIBi_Vals.size), 'k--', label='BEC Sound Speed')
+    ax.set_ylim([0.9 * nu, 1.1 * nu])
+    ax.legend()
+    ax.set_title('Velocity Comparison')
+    ax.set_xlabel(r'$a_{IB}^{-1}$')
+    plt.show()
 
     # # # PHONON MODE CHARACTERIZATION (SPHERICAL)
 
